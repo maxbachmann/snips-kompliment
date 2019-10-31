e814650efe22ec6a92a4d408c785f2301c27d705
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-10-31 14:42:33 +0100

    use python3

--- action-maxbachmann-Kompliment.py
+++ action-maxbachmann-Kompliment.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
-
-import ConfigParser
+import configparser
 import io
 import paho.mqtt.client as mqtt
 import random
@@ -11,7 +9,7 @@ import json
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
+class SnipsConfigParser(configparser.ConfigParser):
     def to_dict(self):
         return {section: {option_name: option for option_name, option in self.items(section)} for section in self.sections()}
 
@@ -20,7 +18,7 @@ def read_configuration_file(configuration_file):
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
             conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
+            conf_parser.read_file(f)
             return conf_parser.to_dict()
     except (IOError, ConfigParser.Error) as e:
         return dict()
@@ -54,8 +52,6 @@ def message(client, userdata, msg):
 def say(session_id, text):
     mqtt_client.publish('hermes/dialogueManager/endSession',
                         json.dumps({'text': text, "sessionId": session_id}))
-
-
 
 
 if __name__ == "__main__":
